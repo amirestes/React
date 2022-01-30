fd2cb77b663b2d965d6579a622f9910081025a6d
amirestes <amirestes@gmail.com>
2022-01-29 20:14:53 -0600

    1

---
+++ Project_1/list.js
@@ -0,0 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function List() {
+    return (
+    <div>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+    )
+}
+
+ReactDOM.render(<List />, document.getElementById("root"))
