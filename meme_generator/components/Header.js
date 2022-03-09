c1cf31f9046c74105ad4d90c8292a20232d9f111
amirestes <amirestes@gmail.com>
2022-03-08 20:18:14 -0600

    added form

--- meme_generator/components/Header.js
+++ meme_generator/components/Header.js
@@ -2,15 +2,13 @@ import React from "react"
 
 export default function Header() {
     return (
-        <header>
-            <div className="header--logo">
-                <img 
-                    src="../images/troll-face.png"
-                    alt="Header image"
-                />
-                <h1>Meme Generator</h1>
-            </div>
-            <h2>React Course - Project 3</h2>
+        <header className="header">
+            <img 
+                src="./images/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
 }
